f92885412b6065be2b254900f0023dba7a210c38
laspe <musa.laspe@gmail.com>
2017-04-19 21:21:27 +0300

    muutos

--- js/main.js
+++ js/main.js
@@ -1,30 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-var myFile = 0;
-var jsonobject = 0;    
-    
-    
-$("button").click(function () {
-    
-    var myFile = document.getElementById("myFile"); // Get input file
-    
-    /* Convert csv to json and put main.js process */
-    
-    $.ajax({
-    url: myFile,
-    async: false,
-    success: function (csvd) {
-        var items = $.csv.toObjects(csvd);
-        jsonobject = JSON.stringify(items);
-        console.log(jsonobject);
-    },
-    dataType: "text",
-    complete: function () {
-        // call a function on complete 
-    }
+    var myFile = 0; // input filen muuttuja
+    var jsonobject = 0; // json datan muuttuja
+
+    /* Admin valikossa olevat tiedoston lataus painikkeet */
+
+    $("button").click(function () {
+
+        var myFile = document.getElementById("myFile"); // Get input file
+
+        /* Convert csv to json and put main.js process */
+
+        csv2geojson.csv2geojson(myFile, {
+            latfield: 'latitude',
+            lonfield: 'longitude',
+            delimiter: ','
+        }, function (err, data) {
+            console.log(err); // err has any parsing errors
+            console.log(data); // data is the data.
+        });
+
+        /* Pelkkä csv to json */
+
+        /*        $.ajax({
+                    url: myFile,
+                    async: false,
+                    success: function (csvd) {
+                        var items = $.csv.toObjects(csvd);
+                        jsonobject = JSON.stringify(items);
+                        alert(jsonobject);
+                    },
+                    dataType: "text",
+                    complete: function () {
+                        // call a function on complete 
+                    }
+
+                });
+        */
 
     });
-    
-});
-    
+
 });
