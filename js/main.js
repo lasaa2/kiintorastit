d3bf0a5bd4bb3141a58c219c53720f0d09a3a9f0
laspe <musa.laspe@gmail.com>
2017-05-02 15:11:18 +0300

    parannusta 1

--- js/main.js
+++ js/main.js
@@ -14,13 +14,44 @@ $(document).ready(function () {
 
     });
 
-    // Päivitys clikki funktio
+    // Päivitys clikki funktio, kun painetaan lataa napista kone suorittaa tämän klikkifunktion
     $("#upload").click(function () {
         var myFile = document.getElementById("myFile").value; // Get input file
         // Parse CSV string
         var data = Papa.parse(myFile);
         alert("Tämä ei tallennu tällä hetkellä mihinkään: \n\n" + JSON.stringify(data));
-
     });
 
+    //});
+
+    /* Funktio joka tulostaa json objektit kartalle. For-looppi. */
+
+    var points = "aineisto.json"; // Muuttuja jossa on päivitetty kiintorastiaineisto
+    
+    $.getJSON(points, function (json) {
+        console.log(points.length);
+        for (var i = 0; i < points.length; i++) {
+
+            //console.log(json[i].KARTTA + "," + json[i].LAT + "," + json[i].LON);
+
+            // Luodaan muuttuja jossa on markkereitten tiedot
+            var markerProperties = {
+                position: new google.maps.LatLng(json[i].LAT, json[i].LON),
+                map: map,
+                animation: google.maps.Animation.DROP,
+                icon: markerImage,
+                infoString: '<div class="markerikkuna"><b style="font-size: 18px;">' + json[i].KARTTA + '</b><br><b>Seura: </b>' + json[i].SEURA + '<br><b>Hinta: </b>' + json[i].HINTA + '<br><b>Mistä kartta? </b>' + json[i].LISAA + '<br><b>MOBO: </b>' + json[i].MOBO + '<br><br><button type="button" class="btn .btn-info"><a href="' + json[i].WEB + '" target=_blank">Lisäinfo</a></button></div>'
+            };
+
+            var marker = new google.maps.Marker(markerProperties);
+            var infowindow = new google.maps.InfoWindow({});
+
+            // Tehdään click funktio joka tulostaa infoikkunan klikattaessa markkeria
+            marker.addListener('click', function () {
+                infowindow.setContent(this.infoString);
+                infowindow.open(map, this);
+                map.setCenter(this.getPosition());
+            });
+        };
+    });
 });
--- js/map.js
+++ js/map.js
@@ -4,9 +4,10 @@ var centerLatLng = {
     lat: 63.344420,
     lng: 26.464051
 };
-var zoomTaso = 5;
+var zoomTaso = 5; // Kartan zoomitaso
+var markerImage = 'img/rastilippu.png'; // Marker image
 
-var points = "../aineisto.json"; // Muuttuja jossa on päivitetty kiintorastiaineisto
+/* Funktio joka muodostaa kartan */
 
 function initMap() {
 
@@ -16,79 +17,26 @@ function initMap() {
         center: new google.maps.LatLng(centerLatLng),
         zoom: zoomTaso,
         disableDefaultUI: true,
-        mapTypeControl: true,
-          mapTypeControlOptions: {
-              style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-              position: google.maps.ControlPosition.TOP_LEFT_CENTER
-          },
-          zoomControl: true,
-          zoomControlOptions: {
-              position: google.maps.ControlPosition.RIGHT_CENTER
-          },
-          scaleControl: true,
-          streetViewControl: false,
-          streetViewControlOptions: {
-              position: google.maps.ControlPosition.LEFT_TOP
-          },
-          fullscreenControl: true,
+        mapTypeControl: false,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.TOP_LEFT_CENTER
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER,
+            style: google.maps.ZoomControlStyle.LARGE,
+        },
+        scaleControl: true,
+        streetViewControl: false,
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP
+        },
+        fullscreenControl: false,
     };
 
     map = new google.maps.Map(mapCanvas, mapProperties);
 
     map = new google.maps.Map($("#map")[0], mapProperties);
 
-    /* Funktio joka tulostaa json objektit kartalle. For-looppi. */
-
-    $.getJSON(points, function (json) {
-            for (var i = 0; i < points.length; i++) {
-
-                console.log(json[i].KARTTA + "," + json[i].LAT + "," + json[i].LON);
-
-                // Luodaan muuttuja jossa on markkereitten tiedot
-                var markerProperties = {
-                    position: new google.maps.LatLng(json[i].LAT, json[i].LON),
-                    map: map,
-                    infoString: 'Kiintorastikartta: ' + json[i].KARTTA + '<br>Seura: ' + json[i].SEURA + '<br>Mistä kartta? ' + json[i].LISAA + '<br><a href="' + json[i].WEB + '" target=_blank">Linkki karttaan</a>'
-                };
-
-                var marker = new google.maps.Marker(markerProperties);
-                var infowindow = new google.maps.InfoWindow({});
-
-                // Tehdään click funktio joka tulostaa infoikkunan klikattaessa markkeria
-                marker.addListener('click', function () {
-                    infowindow.setContent(this.infoString);
-                    infowindow.open(map, this);
-                    map.setCenter(this.getPosition());
-                });
-            };
-        });
-
-
-
-
 };
-
-
-/* Pohjakartan tyylit */
-
-/*
-    var stylesArray = [
-        {
-            featureType: '',
-            elementType: '',
-            stylers: [
-                {
-                    color: ''
-                },
-                {
-                    visibility: ''
-                },
-      // Add any stylers you need.
-    ]
-  },
-        {
-            featureType: '',
-            // Add the stylers you need.
-  }
-]
-*/
