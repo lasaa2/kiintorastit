bc38374c25876be64536dcd30659b0c95ea4b299
laspe <musa.laspe@gmail.com>
2017-05-03 11:14:04 +0300

    new search box with autocomplete

--- js/map.js
+++ js/map.js
@@ -1,11 +1,15 @@
 var map;
 var infowindow;
 var centerLatLng = {
-    lat: 64.250533,  
+    lat: 64.250533,
     lng: 26.476404
 };
 var zoomTaso = 5; // Kartan zoomitaso
 var markerImage = 'img/rastilippu.png'; // Marker image
+
+
+
+
 
 /* Funktio joka muodostaa kartan */
 
@@ -17,6 +21,7 @@ function initMap() {
         center: new google.maps.LatLng(centerLatLng),
         zoom: zoomTaso,
         disableDefaultUI: true,
+        mapTypeId: 'terrain',
         mapTypeControl: false,
         mapTypeControlOptions: {
             style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
@@ -35,8 +40,84 @@ function initMap() {
         fullscreenControl: false,
     };
 
+
     map = new google.maps.Map(mapCanvas, mapProperties);
 
     map = new google.maps.Map($("#map")[0], mapProperties);
+
+    var card = document.getElementById('pac-card');
+    var input = document.getElementById('searchBox');
+    var countries = document.getElementById('country-selector');
+
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    // Set initial restrict to the greater list of countries.
+    autocomplete.setComponentRestrictions({
+        'country': ['fi']
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+    var marker = new google.maps.Marker({
+        map: map,
+        //anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    autocomplete.addListener('place_changed', function () {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17); // Why 17? Because it looks good.
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+        /*
+                infowindowContent.children['place-icon'].src = place.icon;
+                infowindowContent.children['place-name'].textContent = place.name;
+                infowindowContent.children['place-address'].textContent = address;
+                infowindow.open(map, marker);
+        */
+    });
+
+    /*
+        // Sets a listener on a given radio button. The radio buttons specify
+        // the countries used to restrict the autocomplete search.
+        function setupClickListener(id, countries) {
+            var radioButton = document.getElementById(id);
+            radioButton.addEventListener('click', function () {
+                autocomplete.setComponentRestrictions({
+                    'country': countries
+                });
+            });
+        }
+
+        setupClickListener('changecountry-usa', 'us');
+        setupClickListener(
+            'changecountry-usa-and-uot', ['us', 'pr', 'vi', 'gu', 'mp']);
+    */
 
 };
